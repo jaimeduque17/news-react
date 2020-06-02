2dff80cafcb5d5eac4e5c7fb4f70c6aa211eaca5
Jaime Duque <jaime.duque@estratek.com.co>
2020-06-01 19:24:49 -0500

    the form is created

---
+++ src/components/Form.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './Form.module.css';
+
+// JS don't accept class names with hyphen with module sintax. example: btn-block' so the way to use that is ['btn-block']
+
+const Form = () => {
+    return (  
+        <div className={`${styles.searcher} row`}>
+            <div className="col s12 m8 offset-m2">
+                <form>
+                    <h2 className={styles.heading}>Find News by Category</h2>
+                    <div className="input-field col s12">
+                        <input 
+                            type="submit"
+                            className={`${styles['btn-block']} btn-large amber darken-2`}
+                            value="Search"
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+ 
+export default Form;
